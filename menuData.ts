4299820e101a2823aecc7a1ed4c37dc89fdb55fc
yujin mac <kimyu28jin@gmail.com>
2021-07-16 22:11:04 +0900

    메뉴 data 추가

---
+++ menuData.ts
@@ -0,0 +1,16 @@
+export const menuData = {
+  intro: [
+    { id: 1, name: "운영진 소개", location: "/" },
+    { id: 2, name: "오시는 길", location: "/" },
+  ],
+  business: [{ id: 3, name: "주요 사업", location: "/" }],
+  opened: [
+    { id: 4, name: "공지사항", location: "/" },
+    { id: 5, name: "재정보고", location: "/" },
+    { id: 6, name: "이번달 감사한 분", location: "/" },
+  ],
+  volunteer: [
+    { id: 7, name: "봉사 소개", location: "/" },
+    { id: 8, name: "후원 문의", location: "/" },
+  ],
+};
